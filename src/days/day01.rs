ff94db95d710831b6bfe2f7530bd73ff28f4365f
NASCC <pedro.main.tec@gmail.com>
2025-03-12 17:51:48 -0300

    [UPDATED] Solve 2 done

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -30,3 +30,38 @@ pub fn s1(input: Vec<String>) -> i32 {
         .map(|(l, r)| (l - r).abs())
         .sum()
 }
+
+// Day 01: Solve 1
+pub fn s2(input: Vec<String>) -> i32 {
+    // TODO:
+    //  1. create a HashMap (store left items)
+    //  2. create a vector (store right items)
+    //  3. search for each item at right in HashMap by the item key
+    //  4. increase the Hash node value +1 if successly founded
+    //  5. multiply each key by his value
+    //  6. sum all the values
+    use std::collections::HashMap;
+    // NOTE: (1)
+    let mut left_map: HashMap<i32, i32> = HashMap::new();
+    // NOTE: (2)
+    let right_items: Vec<i32> = input
+        .into_iter()
+        .map(|row| {
+            let mut pair = row
+                .split("   ")
+                .map(|item| item.trim().parse::<i32>().unwrap())
+                .into_iter();
+            left_map.insert(pair.next().unwrap(), 0);
+            pair.next().unwrap()
+        })
+        .collect();
+    // NOTE: (3)
+    for item in right_items.iter() {
+        // NOTE: (4)
+        if let Some(key) = left_map.get_mut(item) {
+            *key += 1;
+        }
+    }
+    // NOTE: (5, 6)
+    left_map.iter().map(|(k, v)| k * v).sum()
+}
