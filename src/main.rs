3194365bb70f846b034e21b50a5bd98bafb7917e
NASCC <pedro.main.tec@gmail.com>
2025-03-12 17:52:52 -0300

    [UPDATED] Calling solve (day 01 - part 2) on main

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ use utils as u;
 
 fn main() {
     let input = u::get_file_content("inputs/day01.txt");
-    let result = d01::s1(input);
+    let result = d01::s2(input);
     println!("Result: {}", result);
 }
