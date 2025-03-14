78d678349e6fd5e7f3fc8e88dd0364f2a0f67e14
NASCC <pedro.main.tec@gmail.com>
2025-03-14 14:07:20 -0300

    [UPDATED] Adding test cases + small fix on solve2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -42,7 +42,7 @@ pub fn s2(input: Vec<String>) -> i32 {
     //  6. sum all the values
     use std::collections::HashMap;
     // NOTE: (1)
-    let mut left_map: HashMap<i32, i32> = HashMap::new();
+    let mut left_map: HashMap<i32, (i32, i32)> = HashMap::new();
     // NOTE: (2)
     let right_items: Vec<i32> = input
         .into_iter()
@@ -51,17 +51,48 @@ pub fn s2(input: Vec<String>) -> i32 {
                 .split("   ")
                 .map(|item| item.trim().parse::<i32>().unwrap())
                 .into_iter();
-            left_map.insert(pair.next().unwrap(), 0);
+            let first = pair.next().unwrap();
+            if let Some((left_times, _)) = left_map.get_mut(&first) {
+                *left_times += 1;
+            } else {
+                left_map.insert(first, (1, 0));
+            }
             pair.next().unwrap()
         })
         .collect();
+
     // NOTE: (3)
     for item in right_items.iter() {
         // NOTE: (4)
-        if let Some(key) = left_map.get_mut(item) {
-            *key += 1;
+        if let Some((_, r_times)) = left_map.get_mut(item) {
+            *r_times += 1;
         }
     }
     // NOTE: (5, 6)
-    left_map.iter().map(|(k, v)| k * v).sum()
+    left_map.iter().map(|(k, v)| k * v.0 * v.1).sum()
+}
+
+#[cfg(test)]
+mod day01_tests {
+    use super::{s1, s2};
+
+    #[test]
+    fn solve1_test() {
+        let input: Vec<String> = ["3   4", "4   3", "2   5", "1   3", "3   9", "3   3"]
+            .iter()
+            .map(|row| row.to_string())
+            .collect();
+        let result = s1(input);
+        assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn solve2_test() {
+        let input: Vec<String> = ["3   4", "4   3", "2   5", "1   3", "3   9", "3   3"]
+            .iter()
+            .map(|row| row.to_string())
+            .collect();
+        let result = s2(input);
+        assert_eq!(result, 31);
+    }
 }
