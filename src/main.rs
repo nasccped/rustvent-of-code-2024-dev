e367804cda77c998ee5d6c3e81908518591e79b0
NASCC <pedro.main.tec@gmail.com>
2025-03-14 15:48:05 -0300

    [UPDATED] Callign day 03 on main

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,19 @@ mod days;
 mod temp;
 mod utils;
 
-use days::{day01 as d01, day02 as d02, day04 as d04};
+use days::{day01 as d01, day02 as d02, day03 as d03, day04 as d04};
 use temp::{DayMap, DayMapTrait, DaySolve};
 
 fn main() {
     let solves: &[(DaySolve, DaySolve)] = &[
         (Some(d01::s1), Some(d01::s2)),
         (Some(d02::s1), Some(d02::s2)),
-        (None, None),
+        (Some(d03::s1), Some(d03::s2)),
         (Some(d04::s1), Some(d04::s2)),
     ];
 
     let advent_map = DayMap::new_advent(solves);
-    let (targ_day, targ_solve) = (1, 2);
+    let (targ_day, targ_solve) = (3, 2);
     println!("Result for day {}, solve {}", targ_day, targ_solve);
     let result = advent_map.run(targ_day, targ_solve);
     println!(
