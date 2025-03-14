fcce2b91ae92b2ec699d39ec2734f1a6b98620a4
NASCC <pedro.main.tec@gmail.com>
2025-03-14 14:45:40 -0300

    [UPDATED] Test cases added

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -54,3 +54,42 @@ pub fn s2(input: Vec<String>) -> i32 {
         })
         .count() as i32
 }
+
+#[cfg(test)]
+mod day02_tests {
+    use super::{s1, s2};
+
+    #[test]
+    fn solve1_test() {
+        let input: Vec<String> = [
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9",
+        ]
+        .into_iter()
+        .map(|row| row.to_string())
+        .collect();
+        let result = s1(input);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn solve2_test() {
+        let input: Vec<String> = [
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9",
+        ]
+        .into_iter()
+        .map(|row| row.to_string())
+        .collect();
+        let result = s2(input);
+        assert_eq!(result, 4);
+    }
+}
