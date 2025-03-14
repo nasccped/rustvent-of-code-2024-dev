6ff7e32bddcc28d12ca4430cc303bf7b2355b522
NASCC <pedro.main.tec@gmail.com>
2025-03-14 12:45:11 -0300

    [UPDATED] Calling day 04 solve 02 on main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod days;
 mod utils;
 
-use days::{day01 as d01, day02 as d02};
+use days::{day01 as d01, day02 as d02, day04 as d04};
 use utils as u;
 
 fn main() {
-    let input = u::get_file_content("inputs/day02.txt");
-    let result = d02::s2(input);
+    let input = u::get_file_content("inputs/day04.txt");
+    let result = d04::s2(input);
     println!("Result: {}", result);
 }
