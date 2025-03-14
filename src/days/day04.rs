b6d34fc30473903b156bb0c044d01e23a1e605e6
NASCC <pedro.main.tec@gmail.com>
2025-03-14 12:43:20 -0300

    [UPDATED] Day 04 part 2 done

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,3 +1,57 @@
+pub fn s1(input: Vec<String>) -> i32 {
+    let input: Vec<String> = input
+        .into_iter()
+        .map(|row| row.trim().to_string())
+        .collect();
+
+    let mut accum = 0;
+
+    for i in 0..(input.len()) {
+        for j in 0..(input[i].len()) {
+            let hor: String = input[i].chars().skip(j).take(4).collect();
+            let ver: String = input
+                .iter()
+                .skip(i)
+                .take(4)
+                .map(|row| row.chars().nth(j).unwrap())
+                .collect();
+
+            let _ = [hor, ver]
+                .iter()
+                .filter(|&word| word == "XMAS" || word == "SAMX")
+                .for_each(|_| accum += 1);
+        }
+    }
+
+    for y in 0..(input.len()) {
+        if y > input.len() - 4 {
+            break;
+        }
+        for x in 0..(input[y].len()) {
+            if x > input[y].len() - 4 {
+                break;
+            }
+            let mut diagoes = String::new();
+            let mut diaback = String::new();
+            for i in 0..4 {
+                diagoes.push(input[y + i].chars().nth(x + i).unwrap());
+                diaback.push(
+                    input[y + i]
+                        .chars()
+                        .nth(input[y].len() - x - i - 1)
+                        .unwrap(),
+                );
+            }
+
+            let _ = [diagoes, diaback]
+                .iter()
+                .filter(|&word| word == "XMAS" || word == "SAMX")
+                .for_each(|_| accum += 1);
+        }
+    }
+    accum
+}
+
 pub fn s2(input: Vec<String>) -> i32 {
     let input: Vec<String> = input
         .into_iter()
