22052688ef3e03e6a8caa9960a9dd72db560b906
NASCC <pedro.main.tec@gmail.com>
2025-03-14 21:29:56 -0300

    [UPDATED] Calling it on main

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod days;
 mod temp;
 mod utils;
 
-use days::{day01 as d01, day02 as d02, day03 as d03, day04 as d04};
+use days::{day01 as d01, day02 as d02, day03 as d03, day04 as d04, day05 as d05};
 use temp::{DayMap, DayMapTrait, DaySolve};
 
 fn main() {
@@ -11,10 +11,11 @@ fn main() {
         (Some(d02::s1), Some(d02::s2)),
         (Some(d03::s1), Some(d03::s2)),
         (Some(d04::s1), Some(d04::s2)),
+        (Some(d05::s1), None),
     ];
 
     let advent_map = DayMap::new_advent(solves);
-    let (targ_day, targ_solve) = (3, 2);
+    let (targ_day, targ_solve) = (5, 1);
     println!("Result for day {}, solve {}", targ_day, targ_solve);
     let result = advent_map.run(targ_day, targ_solve);
     println!(
