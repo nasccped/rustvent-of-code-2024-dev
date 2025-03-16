0a269f3f736ba5af6f7da1c01059f937ee29cde0
NASCC <pedro.main.tec@gmail.com>
2025-03-16 12:01:01 -0300

    [UPDATED] New util (row trim)

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,9 @@
 use std::fs;
 
+pub trait VecStringTrim {
+    fn foreach_trim(&self) -> Self;
+}
+
 pub trait StrArrVecString {
     // allow dead code. `into_vecstring` used only in test modules
     #![allow(dead_code)]
@@ -9,6 +13,12 @@ pub trait StrArrVecString {
 impl<const N: usize> StrArrVecString for [&str; N] {
     fn into_vecstring(&self) -> Vec<String> {
         self.iter().map(|row| row.to_string()).collect()
+    }
+}
+
+impl VecStringTrim for Vec<String> {
+    fn foreach_trim(&self) -> Vec<String> {
+        self.into_iter().map(|row| row.trim().to_string()).collect()
     }
 }
 
