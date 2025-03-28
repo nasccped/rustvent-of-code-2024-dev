39009ecef869c2cbb488b0f38422ca4a0e5e17fc
NASCC <pedro.main.tec@gmail.com>
2025-03-27 21:50:58 -0300

    [UPDATED] Day 04 solves enhancement

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,52 +1,100 @@
 use regex::Regex;
 
+const ONLY_MULS_REGEX: &str = r"mul\([0-9]+,[0-9]+\)";
+const MULS_DO_DONTS_REGEX: &str = r"do\(\)|mul\([0-9]+,[0-9]+\)|don't\(\)";
+const NUM_REGEX: &str = r"[0-9]+";
+
+struct PatternStruct {
+    self_value: String,
+}
+
+impl PatternStruct {
+    fn from_str<'a>(input: &'a str) -> Self {
+        Self {
+            self_value: input.to_string(),
+        }
+    }
+
+    fn is_do(&self) -> bool {
+        self.self_value == "do()"
+    }
+
+    fn is_dont(&self) -> bool {
+        self.self_value == "don't()"
+    }
+
+    fn get_mul_value(&self) -> i64 {
+        if self.is_do() || self.is_dont() {
+            0
+        } else {
+            self.find_self_values().into_iter().product()
+        }
+    }
+
+    fn find_self_values(&self) -> Vec<i64> {
+        let num_finder = Regex::new(NUM_REGEX).unwrap();
+
+        num_finder
+            .find_iter(&self.self_value)
+            .map(|val| val.as_str().parse().unwrap())
+            .collect()
+    }
+}
+
+trait PatternStructEval {
+    fn evaluate_from_vec(&self, dont_flag: &mut bool) -> i64;
+}
+
+impl PatternStructEval for Vec<PatternStruct> {
+    fn evaluate_from_vec(&self, dont_flag: &mut bool) -> i64 {
+        let mut accum = 0;
+
+        for pattern in self {
+            if pattern.is_dont() {
+                *dont_flag = true;
+                continue;
+            }
+            if pattern.is_do() {
+                *dont_flag = false;
+                continue;
+            }
+            if *dont_flag {
+                continue;
+            }
+            accum += pattern.get_mul_value();
+        }
+        accum
+    }
+}
+
 pub fn s1(input: Vec<String>) -> i64 {
-    let mulre = Regex::new(r"mul\([0-9]+,[0-9]+\)").unwrap();
-    let numre = Regex::new(r"[0-9]+").unwrap();
-    let mut accum = 0;
+    let mul_pattern = Regex::new(ONLY_MULS_REGEX).unwrap();
+    let mut accum: i64 = 0;
 
     for row in input.iter() {
-        let muls: Vec<&str> = mulre.find_iter(row).map(|m| m.as_str()).collect();
-        accum += muls
-            .iter()
-            .map(|m| {
-                numre
-                    .find_iter(m)
-                    .map(|val| val.as_str().parse::<i64>().unwrap())
-                    .product::<i64>()
-            })
-            .sum::<i64>()
+        let vec_muls: Vec<PatternStruct> = mul_pattern
+            .find_iter(row)
+            .map(|m| PatternStruct::from_str(m.as_str()))
+            .collect();
+
+        accum += vec_muls.evaluate_from_vec(&mut false);
     }
     accum
 }
 
 pub fn s2(input: Vec<String>) -> i64 {
-    let mulre = Regex::new(r"do\(\)|mul\([0-9]+,[0-9]+\)|don't\(\)").unwrap();
-    let numre = Regex::new(r"[0-9]+").unwrap();
-    let mut dont_called = false;
+    let pattern = Regex::new(MULS_DO_DONTS_REGEX).unwrap();
+    let mut dont_flag = false;
     let mut accum = 0;
 
     for row in input.iter() {
-        let muls: Vec<&str> = mulre.find_iter(row).map(|m| m.as_str()).collect();
-
-        for m in muls {
-            if m == "don't()" {
-                dont_called = true;
-                continue;
-            }
-            if m == "do()" {
-                dont_called = false;
-                continue;
-            }
-            if dont_called {
-                continue;
-            }
-            accum += numre
-                .find_iter(m)
-                .map(|val| val.as_str().parse::<i64>().unwrap())
-                .product::<i64>();
-        }
+        let pattern_vec: Vec<PatternStruct> = pattern
+            .find_iter(row)
+            .map(|p| PatternStruct::from_str(p.as_str()))
+            .collect();
+        accum += pattern_vec.evaluate_from_vec(&mut dont_flag)
     }
+
     accum
 }
 
