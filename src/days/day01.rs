209ff6fcd02022fa7bdc3115379d89573cf06cdd
Nasccped <pedro.main.tec@gmail.com>
2025-12-09 00:13:43 +0000

    chore: fix day01 conflict

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,59 +1,41 @@
 use std::collections::HashMap;
 
-/// # Solve for challenge 1 at day 1:
-///
-/// Our input is a list of integer pairs. We should separate our input into
-/// elements at left and elements at right. Then, sort both and sum the
-/// distance between each element.
-pub fn s1(input: &str) -> i64 {
-    let lines = input.lines();
-    let (mut left, mut right): (Vec<i64>, Vec<i64>) = lines.map(|row| row.split("   ")).fold(
-        (Vec::new(), Vec::new()),
-        |(mut l, mut r), mut pair| {
-            let mut next = || pair.next().unwrap().parse().unwrap();
+fn get_vecs(input: &str) -> (Vec<isize>, Vec<isize>) {
+    input
+        .lines()
+        .fold((Vec::new(), Vec::new()), |(mut l, mut r), row| {
+            let mut pair = row.split("   ");
+            let mut next = || pair.next().unwrap().parse::<isize>().unwrap();
             l.push(next());
             r.push(next());
             (l, r)
-        },
-    );
+        })
+}
+
+pub fn s1(input: String) -> usize {
+    let (mut left, mut right) = get_vecs(&input);
     left.sort();
     right.sort();
     left.iter()
         .zip(right.iter())
-        .map(|(l, r)| (l - r).abs())
+        .map(|(l, r)| (l - r).unsigned_abs())
         .sum()
 }
 
-/// # Solve for challenge 2 at day 1:
-///
-/// Our input is a list of integer pairs. We should separate our input into
-/// elements at left and elements at right. Then, for each element in left
-/// list, we should count how many times it appears in right list, multiply the
-/// left value by the times, and finally sum all the thing to get the result.
-pub fn s2(input: &str) -> i64 {
-    let lines = input.lines();
-    // NOTE: we'll store it in a HashMap. It's a little complex but pretty
-    // faster than doing nested for loops. The Key-Value pair will be `&str`
-    // and (i64, i64) since the number shouldn't be parsed if it doesn't appear
-    // in left or right list + left list can hold duplicates.
-    let mapping = lines.map(|row| row.split("   ")).fold(
-        HashMap::<&str, (i64, i64)>::new(),
-        |mut map, mut pair| {
-            let mut next = || pair.next().unwrap();
-            map.entry(next())
-                .and_modify(|v| v.0 += 1)
-                .or_insert_with(|| (1, 0));
-            map.entry(next())
-                .and_modify(|v| v.1 += 1)
-                .or_insert_with(|| (0, 1));
-            map
-        },
-    );
-    mapping.into_iter().fold(0, |acc, (k, v)| {
-        if v.0 == 0 || v.1 == 0 {
-            return acc;
-        }
-        acc + (k.parse::<i64>().unwrap() * v.0 * v.1)
+pub fn s2(input: String) -> usize {
+    let hm: HashMap<&str, (usize, usize)> = input.lines().fold(HashMap::new(), |mut hm, row| {
+        let mut curr = row.split("   ");
+        let mut next = || curr.next().unwrap();
+        hm.entry(next())
+            .and_modify(|(l, _)| *l += 1)
+            .or_insert((1, 0));
+        hm.entry(next())
+            .and_modify(|(_, r)| *r += 1)
+            .or_insert((0, 1));
+        hm
+    });
+    hm.iter().fold(0, |accum, (num, (l, r))| {
+        accum + (num.parse::<usize>().unwrap() * l * r)
     })
 }
 
@@ -61,30 +43,37 @@ pub fn s2(input: &str) -> i64 {
 mod tests {
 
     use super::*;
-
-    const INPUT: &str = include_str!("../../inputs/day01.txt");
-    const SAMPLE_1: &str = r#"3   4
+    use crate::utils::InputFile;
+    const INPUT: &str = "3   4
 4   3
 2   5
 1   3
 3   9
-3   3"#;
-    const SAMPLE_2: &str = r#"3   4
-4   3
-2   5
-1   3
-3   9
-3   3"#;
+3   3";
 
     #[test]
     fn solve1_test() {
-        assert_eq!(s1(SAMPLE_1), 11);
-        assert_eq!(s1(INPUT), 1579939);
+        let result = s1(INPUT.into());
+        assert_eq!(result, 11);
     }
 
     #[test]
     fn solve2_test() {
-        assert_eq!(s2(SAMPLE_2), 31);
-        assert_eq!(s2(INPUT), 20351745);
+        let result = s2(INPUT.into());
+        assert_eq!(result, 31);
+    }
+
+    #[test]
+    fn solve1_run() {
+        let input = InputFile::try_from(("inputs", 1)).unwrap();
+        let result = s1(input.content);
+        assert_eq!(result, 1579939);
+    }
+
+    #[test]
+    fn solve2_run() {
+        let input = InputFile::try_from(("inputs", 1)).unwrap();
+        let result = s2(input.content);
+        assert_eq!(result, 20351745);
     }
 }
