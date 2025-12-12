149e8d218291a7b9dba273f82d08130b6fbf700a
Nasccped <pedro.main.tec@gmail.com>
2025-12-12 23:21:41 +0000

    chore: update day 4

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,195 +1,160 @@
-/// Direction enum to define grid (bidimensional array) traversal.
-enum GoDirection {
-    Up,
-    Down,
-    Left,
-    Right,
-    UpLeft,
-    UpRight,
-    DownLeft,
-    DownRight,
-}
+const XMAS: &str = "XMAS";
+const MAS: &str = "MAS";
+const SAM: &str = "SAM";
 
-/// Shortcut struct to handle grid coordinates (as isize, since negative
-/// positions should be also handled).
-#[derive(Clone)]
-struct Coordinates {
-    x: isize,
+struct Next {
     y: isize,
+    x: isize,
 }
 
-impl Coordinates {
-    fn new(x: isize, y: isize) -> Self {
-        Self { x, y }
+impl Default for Next {
+    fn default() -> Self {
+        Self { y: -1, x: -1 }
+    }
+}
+
+impl Next {
+    fn new(y: isize, x: isize) -> Self {
+        Self { y, x }
+    }
+}
+
+struct Coordinate<'a> {
+    y: isize,
+    x: isize,
+    next: &'a Next,
+}
+
+impl<'a> Coordinate<'a> {
+    fn new(y: isize, x: isize, next: &'a Next) -> Self {
+        Self { y, x, next }
     }
 
-    /// Tests if the self `Coordinates` have valid positions (within grid
-    /// scope)
-    fn have_valid_position(&self, grid: &[Vec<char>]) -> bool {
-        match (self.x, self.y) {
-            (x, y) if x < 0 || y < 0 => false,
-            (x, y) if x >= grid[0].len() as isize || y >= grid.len() as isize => false,
-            _ => true,
+    fn go_next(&mut self) -> &mut Self {
+        self.y += self.next.y;
+        self.x += self.next.x;
+        self
+    }
+
+    fn go_previous(&mut self) -> &mut Self {
+        self.y -= self.next.y;
+        self.x -= self.next.x;
+        self
+    }
+}
+
+trait XMasTrait {
+    fn get_char_at<'a>(&self, coord: &'a Coordinate) -> Option<char>;
+    fn y_out_of_range(&self, y: isize) -> bool;
+    fn x_out_of_range(&self, x: isize) -> bool;
+    fn explore_linear<'a>(&self, coord: &'a mut Coordinate, target: &str) -> bool;
+    fn is_x_mas(&self, y: isize, x: isize) -> bool;
+}
+
+impl XMasTrait for Vec<&str> {
+    fn get_char_at<'a>(&self, coord: &'a Coordinate) -> Option<char> {
+        let Coordinate { y, x, .. } = coord;
+        match (*y, *x) {
+            (y, x) if y < 0 || x < 0 => None,
+            (y, x) => self
+                .get(y as usize)
+                .map(|row| row.chars().nth(x as usize))?,
         }
     }
 
-    /// Update the coordinate values based on a Direction enum variant.
-    fn update(&mut self, direction: &GoDirection) {
-        let (x, y) = (self.x, self.y);
-        let (x, y) = match direction {
-            GoDirection::Up => (x, y - 1),
-            GoDirection::Down => (x, y + 1),
-            GoDirection::Left => (x - 1, y),
-            GoDirection::Right => (x + 1, y),
-            GoDirection::UpLeft => (x - 1, y - 1),
-            GoDirection::UpRight => (x + 1, y - 1),
-            GoDirection::DownLeft => (x - 1, y + 1),
-            GoDirection::DownRight => (x + 1, y + 1),
-        };
-        self.x = x;
-        self.y = y;
+    fn y_out_of_range(&self, y: isize) -> bool {
+        y < 0 || y >= self.len() as isize
     }
 
-    /// Get inner values as usize `(x, y)`
-    ///
-    /// # Panic
-    ///
-    /// This function can panic if any of returned values are less than 0.
-    fn unwrap_usizes(&self) -> (usize, usize) {
-        (self.x as usize, self.y as usize)
+    fn x_out_of_range(&self, x: isize) -> bool {
+        x < 0 || x >= self.len() as isize
     }
-}
 
-/// Finds "XMAS" occurrences withing a grid by using recursion.
-fn find_xmas_occurrence(
-    grid: &[Vec<char>],
-    mut grid_pos: Coordinates,
-    word: &[char],
-    word_ind: usize,
-    direction: &GoDirection,
-) -> bool {
-    // good case
-    if word_ind >= word.len() {
-        return true;
-    }
-    // outside grid
-    if !grid_pos.have_valid_position(grid) {
-        return false;
-    }
-    // not the expected char
-    let (x, y) = grid_pos.unwrap_usizes();
-    if grid[y][x] != word[word_ind] {
-        return false;
-    }
-    // update coordinates + use recursion
-    grid_pos.update(direction);
-    find_xmas_occurrence(grid, grid_pos, word, word_ind + 1, direction)
-}
-
-/// Struct for the X-"MAS" thing (part 2).
-struct XMas {
-    center: char,
-    left_up: char,
-    right_up: char,
-    left_down: char,
-    right_down: char,
-}
-
-impl XMas {
-    fn new(grid: &[Vec<char>], y: usize, x: usize) -> Self {
-        let (center, left_up, right_up, left_down, right_down) = (
-            grid[y][x],
-            grid[y - 1][x - 1],
-            grid[y - 1][x + 1],
-            grid[y + 1][x - 1],
-            grid[y + 1][x + 1],
-        );
-        Self {
-            center,
-            left_up,
-            left_down,
-            right_up,
-            right_down,
+    fn explore_linear<'a>(&self, coord: &'a mut Coordinate, target: &str) -> bool {
+        match (self.get_char_at(coord), target) {
+            (_, "") => true,
+            (Some(a), b) if b.chars().next().is_some_and(|c| c == a) => {
+                let r = self.explore_linear(coord.go_next(), &target[1..]);
+                coord.go_previous();
+                r
+            }
+            _ => false,
         }
     }
 
-    fn is_a_perfect_x(&self) -> bool {
-        let ms = ['M', 'S'];
-        match (
-            self.center,
-            self.left_up,
-            self.right_up,
-            self.left_down,
-            self.right_down,
-        ) {
-            (c, ..) if c != 'A' => false,
-            (_, lu, .., rd) if lu == rd || !ms.contains(&lu) || !ms.contains(&rd) => false,
-            (.., ru, ld, _) if ru == ld || !ms.contains(&ru) || !ms.contains(&ld) => false,
-            _ => true,
+    fn is_x_mas(&self, y: isize, x: isize) -> bool {
+        let go_down_right = Next::new(1, 1);
+        let go_down_left = Next::new(1, -1);
+        let mut coord = Coordinate::new(y, x, &go_down_right);
+        if !self.explore_linear(coord.go_previous(), MAS) && !self.explore_linear(&mut coord, SAM) {
+            return false;
         }
+        coord.go_next();
+        coord.next = &go_down_left;
+        self.explore_linear(coord.go_previous(), MAS) || self.explore_linear(&mut coord, SAM)
     }
 }
 
-/// # Solve for challenge 1 at day 4:
-///
-/// We should iter over all grid positions (bidimensional array traversal) and
-/// call a separated `find_xmas_occurrence` function for each possible
-/// direction. Then, add how many calls returned positive (true - means "XMAS"
-/// occurrence found) into our accumulator.
-pub fn s1(input: &str) -> i64 {
-    // define target, grid and all possible directions
-    let target = ['X', 'M', 'A', 'S'];
-    let grid: Vec<Vec<char>> = input.lines().map(|row| row.chars().collect()).collect();
-    let directions = [
-        GoDirection::Up,
-        GoDirection::Down,
-        GoDirection::Left,
-        GoDirection::Right,
-        GoDirection::UpLeft,
-        GoDirection::UpRight,
-        GoDirection::DownLeft,
-        GoDirection::DownRight,
-    ];
-    let (y_range, x_range) = (0..grid.len(), 0..grid[0].len());
+pub fn s1(input: &str) -> usize {
+    let grid: Vec<&str> = input.split("\n").collect();
     let mut accum = 0;
-    let mut coord: Coordinates;
-    for y in y_range.clone() {
-        for x in x_range.clone() {
-            coord = Coordinates::new(x as isize, y as isize);
-            accum += directions
-                .iter()
-                .filter(|d| find_xmas_occurrence(&grid, coord.clone(), &target, 0, d))
-                .count() as i64;
+    let directions = [
+        Next::new(-1, -1),
+        Next::new(-1, 1),
+        Next::new(1, -1),
+        Next::new(1, 1),
+        Next::new(0, -1),
+        Next::new(0, 1),
+        Next::new(1, 0),
+        Next::new(-1, 0),
+    ];
+    let (mut y, mut x) = (0, 0);
+    loop {
+        if grid.y_out_of_range(y) {
+            break;
+        } else if grid.x_out_of_range(x) {
+            y += 1;
+            x = 0;
+            continue;
         }
+        accum += directions
+            .iter()
+            .map(|d| {
+                let mut coord = Coordinate::new(y, x, d);
+                grid.explore_linear(&mut coord, XMAS)
+            })
+            .filter(|b| *b)
+            .count();
+        x += 1;
     }
     accum
 }
 
-/// # Solve for challenge 2 at day 4:
-///
-/// We should iter over all indexes with 1 index gap from the grid wall, then,
-/// check if any of the positions can build a 'X' defined in the AoC question.
-pub fn s2(input: &str) -> i64 {
-    let grid: Vec<Vec<char>> = input.lines().map(|row| row.chars().collect()).collect();
+pub fn s2(input: &str) -> usize {
+    let grid: Vec<&str> = input.split("\n").collect();
     let mut accum = 0;
-    let (y_range, x_range) = (1..(grid.len() - 1), 1..(grid[0].len() - 1));
-    for y in y_range.clone() {
-        for x in x_range.clone() {
-            if XMas::new(&grid, y, x).is_a_perfect_x() {
-                accum += 1
-            }
+    let (mut y, mut x) = (0, 0);
+    loop {
+        if grid.y_out_of_range(y) {
+            break;
+        } else if grid.x_out_of_range(x) {
+            y += 1;
+            x = 0;
+            continue;
         }
+        if grid.is_x_mas(y, x) {
+            accum += 1;
+        }
+        x += 1;
     }
     accum
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
-
-    const INPUT: &str = include_str!("../../inputs/day04.txt");
-    const SAMPLE_1: &str = r#"MMMSXXMASM
+    use crate::inputs::INPUTS;
+    const INPUT: &str = r#"MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
 MSAMASMSMX
@@ -199,26 +164,16 @@ SMSMSASXSS
 SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX"#;
-    const SAMPLE_2: &str = r#".M.S......
-..A..MSMS.
-.M.S.MAA..
-..A.ASMSM.
-.M.S.M....
-..........
-S.S.S.S.S.
-.A.A.A.A..
-M.M.M.M.M.
-.........."#;
 
     #[test]
-    fn solve1_test() {
-        assert_eq!(s1(SAMPLE_1), 18);
-        assert_eq!(s1(INPUT), 2378);
+    fn part1() {
+        assert_eq!(s1(INPUT), 18);
+        assert_eq!(s1(INPUTS[3]), 2378);
     }
 
     #[test]
-    fn solve2_test() {
-        assert_eq!(s2(SAMPLE_2), 9);
-        assert_eq!(s2(INPUT), 1796);
+    fn part2() {
+        assert_eq!(s2(INPUT), 9);
+        assert_eq!(s2(INPUTS[3]), 1796);
     }
 }
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,15 +1,16 @@
 pub mod day01;
 pub mod day02;
 pub mod day03;
-//pub mod day04;
+pub mod day04;
 //pub mod day05;
 //pub mod day06;
 //pub mod day07;
 
 type SolvePair = (Option<fn(&str) -> usize>, Option<fn(&str) -> usize>);
 
-pub const SOLVES: [SolvePair; 3] = [
+pub const SOLVES: [SolvePair; 4] = [
     (Some(day01::s1), Some(day01::s2)),
     (Some(day02::s1), Some(day02::s2)),
     (Some(day03::s1), Some(day03::s2)),
+    (Some(day04::s1), Some(day04::s2)),
 ];
